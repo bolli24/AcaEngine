6ebd160bdf4ecb9820a87f8ba7da4544ccda3072
bolli24 <4827765-bolli24@users.noreply.gitlab.com>
2021-11-18 15:16:35 +0100

    Sphere rendering fixed

--- src/engine/graphics/renderer/mesh.cpp
+++ src/engine/graphics/renderer/mesh.cpp
@@ -1,6 +1,50 @@
 #include "mesh.hpp"
-#include "mesh.hpp"
 
 namespace graphics {
 
+const std::vector<VertexAttribute> Mesh::attributes = {{PrimitiveFormat::FLOAT, 3},
+                                                       {PrimitiveFormat::FLOAT, 2},
+                                                       {PrimitiveFormat::FLOAT, 3}};
+
+Mesh::Mesh(const utils::MeshData& _meshData)
+    : geometryBuffer(GLPrimitiveType::TRIANGLES, attributes.data(), attributes.size(), 0) {
+    std::vector<float> data;
+
+    const auto& pos = _meshData.positions;
+    const auto& uvs = _meshData.textureCoordinates;
+    const auto& normals = _meshData.normals;
+
+    for (const auto& face : _meshData.faces) {
+        for (const auto& idx : face.indices) {
+            data.push_back(pos[idx.positionIdx][0]);
+            data.push_back(pos[idx.positionIdx][1]);
+            data.push_back(pos[idx.positionIdx][2]);
+
+            if (idx.textureCoordinateIdx.has_value()) {
+                data.push_back(uvs[idx.textureCoordinateIdx.value()][0]);
+                data.push_back(uvs[idx.textureCoordinateIdx.value()][1]);
+            } else {
+                data.push_back(0.f);
+                data.push_back(0.f);
+            }
+
+            if (idx.normalIdx.has_value()) {
+                data.push_back(normals[idx.normalIdx.value()][0]);
+                data.push_back(normals[idx.normalIdx.value()][1]);
+                data.push_back(normals[idx.normalIdx.value()][2]);
+            } else {
+                data.push_back(0.f);
+                data.push_back(0.f);
+                data.push_back(-1.f);
+            }
+        }
+    }
+
+    geometryBuffer.setData(data.data(), data.size() * sizeof(float));
 }
+
+void Mesh::draw() const {
+    geometryBuffer.draw();
+}
+
+}  // namespace graphics
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include <engine/graphics/core/device.hpp>
 #include <engine/graphics/core/geometrybuffer.hpp>
 #include <engine/input/inputmanager.hpp>
-#include <engine/utils/meshloader.hpp>
 #include <engine/graphics/core/opengl.hpp>
 #include <engine/graphics/resources.hpp>
 #include <GLFW/glfw3.h>
@@ -23,12 +22,6 @@
 using namespace std::chrono_literals;
 using namespace graphics;
 
-struct Vertex {
-    glm::vec3 position;
-    glm::vec2 uv;
-    glm::vec3 normal;
-};
-
 int main(int argc, char* argv[]) {
 #ifndef NDEBUG
 #if defined(_MSC_VER)
@@ -44,43 +37,19 @@ int main(int argc, char* argv[]) {
     {
         Camera camera(90.0f, 0.1f, 100.0f);
         camera.setView(glm::lookAt(
-            glm::vec3(0, 2, 2),
+            glm::vec3(0, 0, 2),
             glm::vec3(0, 0, 0),
             glm::vec3(0, 1, 0)));
 
-        auto mesh = utils::MeshLoader::get("models/sphere.obj");
+        auto meshData = utils::MeshLoader::get("models/sphere.obj");
         glm::mat4 meshTransform = glm::mat4(1.0f);
         glm::mat4 meshProjection = meshTransform * camera.getViewProjection();
 
-        std::vector<Vertex> vertices;
+        Mesh mesh(*meshData);
 
-        const auto& pos = mesh->positions;
-        const auto& uvs = mesh->textureCoordinates;
-        const auto& normals = mesh->normals;
-
-        for (const auto& face : mesh->faces) {
-            const auto& idx = face.indices;
-
-            vertices.push_back({pos[idx[0].positionIdx], uvs[idx[0].textureCoordinateIdx.value()], normals[idx[0].normalIdx.value()]});
-            vertices.push_back({pos[idx[1].positionIdx], uvs[idx[1].textureCoordinateIdx.value()], normals[idx[1].normalIdx.value()]});
-            vertices.push_back({pos[idx[2].positionIdx], uvs[idx[2].textureCoordinateIdx.value()], normals[idx[2].normalIdx.value()]});
-        }
-
-        const std::array attributes = {VertexAttribute{PrimitiveFormat::FLOAT, 3},
-                                       VertexAttribute{PrimitiveFormat::FLOAT, 2},
-                                       VertexAttribute{PrimitiveFormat::FLOAT, 3}};
-
-        GeometryBuffer geometryBuffer(GLPrimitiveType::TRIANGLES, attributes.data(), attributes.size(), 0);
-        geometryBuffer.setData(vertices.data(), vertices.size() * sizeof(Vertex));
-        geometryBuffer.bind();
-
-        // sampler
-        // needs to be stored somewhere for as long as the texture exists!
-        static Sampler sampler(Sampler::Filter::LINEAR, Sampler::Filter::LINEAR,
-                               Sampler::Filter::LINEAR, Sampler::Border::MIRROR);
-        // loading
+        static const Sampler sampler(Sampler::Filter::LINEAR, Sampler::Filter::LINEAR,
+                                     Sampler::Filter::LINEAR, Sampler::Border::MIRROR);
         auto texture = Texture2DManager::get("textures/planet1.png", sampler);
-        // binding
         texture->bind(0);
 
         const auto* fragmentShader = ShaderManager::get("shader/demo.frag", ShaderType::FRAGMENT);
@@ -96,7 +65,7 @@ int main(int argc, char* argv[]) {
         while (!glfwWindowShouldClose(window)) {
             glCall(glClear, GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-            geometryBuffer.draw();
+            mesh.draw();
 
             glfwPollEvents();
             glfwSwapBuffers(window);
