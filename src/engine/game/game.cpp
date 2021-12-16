1e6eb306f2abc2c1be223cebc781fb314db85cb5
Henrik Huth <4827765-bolli24@users.noreply.gitlab.com>
2021-12-16 16:53:18 +0100

    Implemeted registry for any component type

--- src/engine/game/game.cpp
+++ src/engine/game/game.cpp
@@ -46,21 +46,15 @@ void Game::run() {
     GLFWwindow* window = graphics::Device::getWindow();
     glCall(glEnable, GL_DEPTH_TEST);
 
-    Registry<float> registry;
-    Entity entity = registry.create();
-    registry.setData(entity, 1.3f);
-    float f = registry.getData(entity);
+    Registry registry;
 
+    Entity entity1 = registry.create();
     Entity entity2 = registry.create();
-    registry.setData(entity2, 2.0f);
-    Entity entity3 = registry.create();
-    registry.setData(entity3, 3.0f);
-    registry.erase(entity2);
-    Entity entity4 = registry.create();
-    registry.setData(entity4, 4.1f);
 
-    float sum = 0.0f;
-    registry.execute(sumUp, sum);
+    auto& positions = registry.getComponents<glm::vec3>();
+    positions.insert(entity1, {0.1f, 1.0f, 0.5f});
+
+    glm::vec3 pos = *positions.at(entity1);
 
     {
         std::unique_ptr<GameState> springstate = std::make_unique<SpringState>();
